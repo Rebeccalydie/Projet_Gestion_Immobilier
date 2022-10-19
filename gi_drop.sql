e3acf056ac6d7e55ef3b61dff32af197b7b63286
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2022-10-19 19:22:40 +0100

    table suppression des tables inachevé

---
+++ gi_drop.sql
@@ -0,0 +1,14 @@
+rem Ce fichier vas permettre de supprimer les differentes tables crée dans notre base de données
+
+
+
+
+
+DROP TABLE proprietaires CASCADE CONSTRAINTS;
+DROP TABLE maisons_vente CASCADE CONSTRAINTS;
+DROP TABLE clients CASCADE CONSTRAINTS;
+DROP TABLE maisons_locations CASCADE CONSTRAINTS;
+DROP TABLE historiques_locations CASCADE CONSTRAINTS;
+DROP TABLE geo_localisation CASCADE CONSTRAINTS;
+DROP TABLE historiques_vente CASCADE CONSTRAINTS;
+
