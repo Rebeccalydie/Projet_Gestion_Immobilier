8e86c4ccdb0de0b18f57a0b732d60d675b1dcaea
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2022-10-24 00:47:54 +0100

    Suppression de certaines tables

--- gi_cre/tables/constraints/historiques_vente.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE historiques_vente
-ADD (CONSTRAINT id_client_pk
-        FOREIGN KEY (id_client)
-        REFERENCES clients(id_client),
-    CONSTRAINT id_vente_nn
-        FOREIGN KEY(id_vente)
-        REFERENCES maisons_vente(id_vente)
-    ;)
