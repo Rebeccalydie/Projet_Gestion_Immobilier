8cba1315c9e19f54e0fb6c21141b703bfe7138ee
Rebeccalydie <115658677+Rebeccalydie@users.noreply.github.com>
2022-10-25 19:22:41 +0200

    Create maison_locations.sql

---
+++ Insertion/maison_locations.sql
@@ -0,0 +1,10 @@
+CREATE TABLE maisons_location
+( 
+  id_location VARCHAR2(20) CONSTRAINT id_location_nn NOT NULL,
+  type_location VARCHAR2(25) NOT NULL,
+  description_location VARCHAR2(255) NOT NULL,
+  adresse_location VARCHAR2(50) NOT NULL,
+  prix_menstruel FLOAT(10) NOT NULL,
+  caution FLOAT(10) NOT NULL,	
+  id_proprietaire VARCHAR2(20) NOT NULL
+); 
