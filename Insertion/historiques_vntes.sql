0321f65081eeeba5717057c1de47266346e9aabd
tchinda yvana <tchindayvana83@gmail.com>
2022-10-26 13:25:54 +0100

    ordre dans le dossier Insertion

--- Insertion/historiques_vntes.sql
+++
@@ -1,8 +0,0 @@
-prompt .....*********  creation de la table historiques_vente *********.......
-
-CREATE TABLE historiques_vente
-(
-    id_client VARCHAR2(20) NOT NULL,
-    date_vente DATE NOT NULL,
-    id_vente VARCHAR(20) NOT NULL
-);
--- Insertion/table_clients.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CLIENTS
-(
-    id_client NUMBER(20) CONSTRAINT id_vente_nn NOT NULL,
-    nom_client VARCHAR2(50) NOT NULL,
-    prenom_client VARCHAR2(50) NOT NULL,
-    email_client VARCHAR2(50) NOT NULL,
-    tel_client NUMBER(20) NOT NULL,
-    cp_client VARCHAR2(25) NOT NULL
-);
