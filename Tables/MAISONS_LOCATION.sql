870b57579404c02986d51fbeb3813922389a9947
versselsiyapze@gmail.com <versselsiyapze@gmail.com>
2022-10-30 01:56:23 -0700

    modification de la table maison location

--- Tables/MAISONS_LOCATION.sql
+++ Tables/MAISONS_LOCATION.sql
@@ -2,12 +2,14 @@ Prompt *********  creation de la table MAISON_LOCATION *************************
 
 CREATE TABLE maisons_location
 ( id_location VARCHAR2(20) CONSTRAINT id_location_nn NOT NULL,
-  type_location VARCHAR2(25),
-  description_location VARCHAR(255),
+  type_location VARCHAR2(25) NOT NULL,
+  description_location VARCHAR(255) NOT NULL,
   adresse_location VARCHAR2(50),
-  prix_menstruel FLOAT (10),
+  prix_menstruel FLOAT (10) NOT NULL,
   caution FLOAT(10),
-  id_client INT 	
+  quartier VARCHAR2 (20),
+  rue VARCHAR2(20) NOT NULL,
+  id_proprietaire INT NOT NULL	
 ); 
 
 CREATE INDEX id_location_nn
