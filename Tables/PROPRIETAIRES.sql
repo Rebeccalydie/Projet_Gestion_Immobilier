c09c1bb43cbe3e77e2752a4891ada8c035f33d08
versselsiyapze@gmail.com <versselsiyapze@gmail.com>
2022-10-30 02:37:11 -0700

    creation de la table pays

--- Tables/PROPRIETAIRES.sql
+++ Tables/PROPRIETAIRES.sql
@@ -6,31 +6,15 @@ Prompt *********  creation de la table PROPRIETAIRES .......
 
 CREATE TABLE proprietaires
 ( id_proprietaire INT CONSTRAINT id_prop_pk NOT NULL,
-  nom_proprio VARCHAR2(50) ,
-  prenom_proprio VARCHAR2(50),
+  nom_proprio VARCHAR2(50) NOT NULL,
+  prenom_proprio VARCHAR2(50) NOT NULL,
   email_proprio VARCHAR2(50),
-  tel_proprio NUMBER(20),
+  tel_proprio NUMBER(20) NOT NULL,
   cp_proprio VARCHAR2(25)
 );
 
 CREATE  INDEX id_prop_pk
 ON proprietaires (id_proprietaire);
-rem ************************************************************************
-rem creation de la table proprietaires
 
-
-Prompt *********  creation de la table proprietaires .......
-
-CREATE TABLE proprietaires
-( id_proprietaire INT NOT NULL,
-  nom_proprio VARCHAR2(20) NOT NULL,
-  prenom_proprio VARCHAR2(20) NULL NULL,
-  email_proprio VARCHAR2(20) NOT NULL,
-  tel_proprio NUMBER (10) NOT NULL,
-  cp_proprio VARCHAR2(20) NOT NULL
-); 
-
-CREATE INDEX id_prop_pk
-ON proprietaires(id_proprietaire);
 
 
