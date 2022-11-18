75d7bb53456a76f82b82811626bc3a4c98234216
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2022-11-18 12:24:12 +0100

    Creation des fichiers insertions villes et suppression villes

---
+++ insertion_ville.sql
@@ -0,0 +1,5 @@
+BEGIN 
+sv_register (p_nom_ville => '&nom_ville',
+             p_id_ville => '&id_ville');
+END;
+/
---
+++ supprimer_ville.sql
@@ -0,0 +1,5 @@
+BEGIN
+DELETE FROM VILLES
+wHERE id_ville = '&id_ville';
+END;
+/
