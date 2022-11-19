11b337be5e1b07babcb7fb4ad094aaea9b69f139
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2022-11-19 10:04:54 +0100

    Last update

--- consulter_details_logement.sql
+++ consulter_details_logement.sql
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 SELECT *
 FROM LOGEMENTS
 WHERE id_logement LIKE '&id_logement';
-=======
-SELECT * FROM LOGEMENTS
-WHERE id_logement = '&id_logement';
->>>>>>> 4c18c538bd3fbd6531a2eda71c1bfc6f4e74b54f
--- procedure/procedure_create_quartiers.sql
+++ procedure/procedure_create_quartiers.sql
@@ -1,21 +1,26 @@
-CREATE OR REPLACE PROCEDURE
-pro_QUARTIERS(pro_id_quartier IN VARCHAR2,
-              pro_id_ville    IN VARCHAR2,
-              pro_nom_quartier IN VARCHAR2)
-IS
-BEGIN
-    INSERT INTO pro_QUARTIERS(id_quartiere,
-                              id_ville, 
-                              nom_quartier)
-SELECT(pro_id_quartier, 
-       pro_id_ville,    
-       pro_nom_quartier)
-FROM DUAL
- WHERE NOT EXISTS 
- (SELECT NULL
-               FROM QUARTIERS
-               WHERE pro_id_quartier = id_quartier
-                     pro_id_ville = id_ville
-                     pro_nom_quartier = nom_quartier);     
-END;
-/
+CREATE OR REPLACE PROCEDURE pro_QUARTIERS(pro_id_quartier IN VARCHAR2,
+                                          pro_id_ville    IN VARCHAR2,
+                                          pro_nom_quartier IN VARCHAR2)
+       IS 
+              l_msg VARCHAR2(100);
+       BEGIN 
+            INSERT INTO QUARTIERS(id_quartier,
+                                  id_ville,
+                                  nom_quartier)
+            SELECT pro_id_quartier,
+                   pro_id_ville,
+                   pro_nom_quartier
+            FROM DUAL 
+            WHERE NOT EXISTS 
+                     (SELECT NULL
+                     FROM QUARTIERS
+                     WHERE pro_id_quartier = id_quartier);
+            l_msg :=
+                     CASE 
+                        WHEN SQL%ROWCOUNT > 0 THEN 'le quartier enregistrer'
+                        ELSE 'le quartier existe deja'
+                        END;
+
+       DBMS_OUTPUT.put_line(l_msg);
+    END;
+    /
